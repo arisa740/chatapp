046e81145ba401974de0dd11afe20465fafa4fef
arisa740 <arisa.camp@bumail.net>
2024-10-12 23:53:55 +0700

    Update tasks_controller.rb

--- tasks_controller.rb
+++ tasks_controller.rb
@@ -1,26 +1,36 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:complete, :destroy]
+
   def index
-    @tasks = Task.where(status: :incomplete)
+    @tasks = Task.where(status: 'incomplete')
     @new_task = Task.new
   end
 
   def create
-    @task = Task.new(task_params)
-    @task.status = :incomplete
-    if @task.save
-      redirect_to tasks_path, notice: "Task created!"
+    @new_task = Task.new(task_params)
+    @new_task.status = 'incomplete'
+    if @new_task.save
+      redirect_to tasks_path
     else
       render :index
     end
   end
 
   def complete
-    @task = Task.find(params[:id])
-    @task.update(status: :complete)
-    redirect_to tasks_path, notice: "Task marked as complete!"
+    @task.update(status: 'complete')
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name)
